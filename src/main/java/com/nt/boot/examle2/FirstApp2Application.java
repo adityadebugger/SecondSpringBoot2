1b16b727ac49665493fbd5b35e4510c6a29b238a
adityadebugger <56907184+adityadebugger@users.noreply.github.com>
2019-10-23 23:07:18 +0530

    SeconfSpringFile

---
+++ src/main/java/com/nt/boot/examle2/FirstApp2Application.java
@@ -0,0 +1,15 @@
+package com.nt.boot.examle2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstApp2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstApp2Application.class, args);
+		
+		System.out.println("First Boot Apps");
+	}
+
+}
---
+++ src/test/java/com/nt/boot/examle2/FirstApp2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nt.boot.examle2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstApp2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
